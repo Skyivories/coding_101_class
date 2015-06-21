b09a6c09e2935abee29fd7136294c80eaaa3f6bf
Jamie Cai <flybycai@goodreads.com>
2015-06-20 21:01:49 -0700

    LOOK A REAL COMMIT MESSAGE hah jk

--- lotr_twist/texttwist.js
+++ lotr_twist/texttwist.js
@@ -58,30 +58,31 @@ $(document).ready(function() {
         $(".lettersField").val("");
       }
       $(".letters").text(selectedText.letters);
+    }
+  });
+
+  $(".lettersField").on("keydown", function(e) {
+    // On backspace, put character back in letters display
+    if (e.keyCode == 8) {
+      var length = $(".lettersField").val().length;
+      var lastChar = $(".lettersField").val().substring(length-1, length);
+      $(".letters").text($(".letters").text() + lastChar);
     } else {
-      // Check that the input is only valid letters
-      var regex = RegExp("[" + selectedText.letters + "]+");
-      var newInputVal = $(".lettersField").val().match(regex);
-      $(".lettersField").val(newInputVal);
-      // If a user types a valid character, remove it from the letters display
+      // Check that the input is only valid letters. If the letters are valid,
+      // remove them from the letters display
       var charCode = e.keyCode;
       var charStr = String.fromCharCode(charCode).toLowerCase();
-      if ($(".letters").text().indexOf(charStr) >= 0) {
+      if ($(".letters").text().indexOf(charStr) < 0) {
+        return false;
+      } else {
         var newLetters = $(".letters").text().replace(charStr, "");
         $(".letters").text(newLetters);
       }
     }
   });
-
-  $(".lettersField").on("keydown", function(e) {
-    if (e.keyCode == 8) {
-      var length = $(".lettersField").val().length;
-      var lastChar = $(".lettersField").val().substring(length-1, length);
-      $(".letters").text($(".letters").text() + lastChar);
-    }
-  });
 });
 
+// Space bar randomizes letters
 $(document).on("keyup", function(e) {
   if (e.keyCode == 32) {
     var word = $(".letters").text();
